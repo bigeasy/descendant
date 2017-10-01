0bed9aecd43fb12f4f6c06ab49f0376fa890607a
Alan Gutierrez <alan@prettyrobots.com>
2017-10-01 02:29:04 -0500

    Initial implementation.

---
+++ descendent.js
@@ -0,0 +1,113 @@
+var cadence = require('extant')
+var util = require('util')
+var events = require('events')
+
+function Descendent (process) {
+    var descendent = this
+    this._process = process
+    this._children = {}
+    this._process.on('message', this._listener = function (message) {
+        var vargs = Array.prototype.slice.call(arguments)
+        if (
+            message.module == 'descendent' &&
+            Array.isArray(message.to) &&
+            Array.isArray(message.path)
+        ) {
+            message = JSON.parse(JSON.stringify(message))
+            message.path.push(descendent._process.pid)
+            if (message.to.length == 0) {
+                vargs[0] = message.body
+                vargs.unshift(message.name, message.path)
+                descendent.emit.apply(descendent, vargs)
+            } else {
+                var entry = descendent._children[message.to[0]]
+                if (entry != null) {
+                    message.to.shift()
+                    vargs[0] = message
+                    entry.child.send.apply(entry.child, vargs)
+                }
+            }
+        } else {
+            descendent.emit.apply(descendent, [ 'down' ].concat(vargs))
+        }
+    })
+    events.EventEmitter.call(this)
+}
+util.inherits(Descendent, events.EventEmitter)
+
+Descendent.prototype.destroy = function () {
+    this._process.removeListener('message', this._listener)
+    Object.keys(this._children).forEach(function (pid) {
+        this.removeChild(this._children[pid].child)
+    }, this)
+}
+
+Descendent.prototype.addChild = function (child, cookie) {
+    var descendent = this
+    var entry = this._children[child.pid] = {
+        child: child,
+        listener: function (message) {
+            var vargs = Array.prototype.slice.call(arguments)
+            if (
+                message.module == 'descendent' &&
+                typeof message.to == 'number' &&
+                Array.isArray(message.path)
+            ) {
+                message = JSON.parse(JSON.stringify(message))
+                message.path.unshift(descendent._process.pid)
+                if (
+                    message.to == descendent._process.pid ||
+                    (message.to == 0 && descendent._process.send == null)
+                ) {
+                    vargs[0] = message.body
+                    vargs.unshift(message.name, cookie)
+                    descendent.emit.apply(descendent, vargs)
+                } else if (descendent._process.send) {
+                    vargs[0] = message
+                    descendent._process.send.apply(descendent._process, vargs)
+                }
+            } else {
+                vargs.unshift('up', cookie)
+                descendent.emit.apply(descendent, vargs)
+            }
+        }
+    }
+    child.on('message', entry.listener)
+}
+
+Descendent.prototype.removeChild = function (child) {
+    var entry = this._children[child.pid]
+    delete this._children[child.pid]
+    entry.child.removeListener('message', entry.listener)
+}
+
+Descendent.prototype.up = function (pid, name, message) {
+    if (this._process.send) {
+        var vargs = Array.prototype.slice.call(arguments, 2)
+        vargs[0] = {
+            module: 'descendent',
+            name: name,
+            to: pid,
+            path: [ this._process.pid ],
+            body: message
+        }
+        this._process.send.apply(this.process, vargs)
+    }
+}
+
+Descendent.prototype.down = function (path, name, message) {
+    var vargs = Array.prototype.slice.call(arguments, 2)
+    var envelope = vargs[0] = {
+        module: 'descendent',
+        name: name,
+        to: path.slice(),
+        path: [],
+        body: message
+    }
+    if (envelope.to[0] == this._process.pid) {
+        envelope.to.shift()
+    }
+    this._listener.apply(null, vargs)
+}
+
+module.exports = Descendent
--- t/descendent.t.js
+++ t/descendent.t.js
@@ -1,6 +1,134 @@
-require('proof')(1, prove)
+require('proof')(9, prove)
 
 function prove (okay) {
     var Descendent = require('..')
-    okay(Descendent, 'require')
+    var events = require('events')
+
+    var expect = [{
+        vargs: [ 2, 1 ],
+        message: 'to top'
+    }, {
+        vargs: [{
+            module: 'descendent',
+            name: 'hello:world',
+            to: 0,
+            path: [ 1 ],
+            body: 1
+        }],
+        message: 'up up and out'
+    }, {
+        vargs: [{
+            module: 'descendent',
+            to: 0,
+            path: [ 1, 2 ],
+            body: 1
+        }],
+        message: 'child up and out'
+    }, {
+        vargs: [ 2, { a: 1 } ],
+        message: 'up message'
+    }, {
+        vargs: [{
+            module: 'descendent',
+            name: 'hello:world',
+            to: [],
+            path: [ 1 ],
+            body: 1
+        }],
+        message: 'parent to child'
+    }, {
+        vargs: [ [ 0, 1 ], 1 ],
+        message: 'down received'
+    }, {
+        vargs: [ 3 ],
+        message: 'down non-descent message'
+    }, {
+        vargs: [{
+            module: 'descendent',
+            name: 'hello:world',
+            to: [ 3 ],
+            path: [ 1 ],
+            body: 1
+        }],
+        message: 'down method message'
+    }, {
+        vargs: [{
+            module: 'descendent',
+            name: 'hello:world',
+            to: [ 3 ],
+            path: [ 1 ],
+            body: 1
+        }],
+        message: 'down method message'
+    }]
+
+    function asExpected (value) {
+        var expected = expect.shift()
+        okay(Array.prototype.slice.call(arguments), expected.vargs, expected.message)
+    }
+
+    // Send messages past the parent that should do nothing.
+    var parent = new events.EventEmitter
+    parent.pid = 1
+    var descendent = new Descendent(parent)
+    descendent.up(0, 'hello:world', 1)
+    var child = new events.EventEmitter
+    child.pid = 2
+    descendent.addChild(child, 2)
+    child.emit('message', { module: 'descendent', to: 3, path: [] })
+
+    // Send a message that defaults to the parent.
+    descendent.on('hello:world', asExpected)
+    child.emit('message', { module: 'descendent', name: 'hello:world', to: 0, path: [ 2 ], body: 1 })
+
+    // Send message up out of parent.
+    var parent = new events.EventEmitter
+    parent.pid = 1
+    parent.send = asExpected
+    var descendent = new Descendent(parent)
+    descendent.up(0, 'hello:world', 1)
+    var child = new events.EventEmitter
+    child.pid = 2
+    descendent.addChild(child, 2)
+    child.emit('message', { module: 'descendent', to: 0, path: [ 2 ], body: 1 })
+
+    // Send a message up and out as a non-descendent message.
+    descendent.on('up', asExpected)
+    child.emit('message', { a: 1 })
+
+    // Send a message down to the child.
+    child.send = asExpected
+    // This one goes nowhere.
+    parent.emit('message', {
+        module: 'descendent',
+        to: [ 3 ],
+        path: [],
+        name: 'hello:world',
+        body: 1
+    })
+    // This one goes to the child.
+    parent.emit('message', {
+        module: 'descendent',
+        to: [ 2 ],
+        path: [],
+        name: 'hello:world',
+        body: 1
+    })
+    // This arrives at the descendent.
+    descendent.on('hello:world', asExpected)
+    parent.emit('message', {
+        module: 'descendent',
+        to: [],
+        path: [ 0 ],
+        name: 'hello:world',
+        body: 1
+    })
+    // Non-descendent message coming down.
+    descendent.on('down', asExpected)
+    parent.emit('message', 3)
+
+    descendent.down([ 1, 2, 3 ], 'hello:world', 1)
+    descendent.down([ 2, 3 ], 'hello:world', 1)
+
+    descendent.destroy()
 }
